abedc3e7cd09c2710b0571ee0bc7f8d248dea0a3
damar-ops <symwalmghrb@gmail.com>
2026-09-15 23:15:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212728867295;
+global.pairingNumber = 212728867205;
 global.owner = [
   ['212646565333', 'DAMAR-MD', true],
   ['', 'Owner 2', true],
